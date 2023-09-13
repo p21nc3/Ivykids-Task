bc7b8ddb3c765ac0f2fcf13071279373f7741cdb
p21nc3 <pp4768@srmist.edu.in>
2023-09-14 02:20:02 +0530

    commit err..

---
+++ src/utils/constants.js
@@ -0,0 +1,64 @@
+export const logoImageURL =
+  "https://res.cloudinary.com/dohfgct6b/image/upload/v1694628271/Ivykids-Task/Ivykids-task_cqxoa3.png";
+
+export const loginPageHeroImg =
+  "https://res.cloudinary.com/dohfgct6b/image/upload/v1694630896/Ivykids-Task/2023-09-14_00_17_03_nphsgt.png";
+
+
+
+export const defaultBgImage =
+  "https://res.cloudinary.com/dxnbnviuz/image/upload/w_400,f_auto,q_auto/v1687604239/socialMedia/ivykids-default-bg_deqxio.png";
+
+export const dummyProfileImage =
+  "https://res.cloudinary.com/dxnbnviuz/image/upload/v1687603602/socialMedia/AvatarTwo_isbynr.png";
+
+export const errorImage =
+  "https://res.cloudinary.com/dxnbnviuz/image/upload/v1687994901/socialMedia/Oops_404_Error_with_a_broken_robot-cuate_lzgce5.svg";
+
+export const avatarImages = [
+  "https://res.cloudinary.com/dxnbnviuz/image/upload/v1687603622/socialMedia/AvatarSix_wqf4ya.png",
+  "https://res.cloudinary.com/dxnbnviuz/image/upload/v1687603611/socialMedia/AvatarFour_otab93.png",
+  "https://res.cloudinary.com/dxnbnviuz/image/upload/v1687603607/socialMedia/AvatarThree_vyhlpx.png",
+  "https://res.cloudinary.com/dxnbnviuz/image/upload/v1687603615/socialMedia/AvatarFive_siczhk.png",
+  "https://res.cloudinary.com/dxnbnviuz/image/upload/v1687603596/socialMedia/AvatarOne_a3knbz.png",
+  "https://res.cloudinary.com/dxnbnviuz/image/upload/v1687603602/socialMedia/AvatarTwo_isbynr.png",
+];
+
+export const guestUserLoginDetails = {
+  username: "guestuser",
+  password: "guestUserLoginDetails",
+};
+
+export const styles = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  bgcolor: "background.paper",
+  boxShadow: 24,
+  p: 4,
+};
+
+export const actionTypes = {
+  GET_ALL_POSTS: "GET_ALL_POSTS",
+  LIKE_POST: "LIKE_POST",
+  DISLIKE_POST: "DISLIKE_POST",
+  FILTER_POSTS: "FILTER_POSTS",
+  GET_ALL_USERS: "GET_ALL_USERS",
+  GET_ALL_BOOKMARKS: "GET_ALL_BOOKMARKS",
+  ADD_BOOKMARK: "ADD_BOOKMARK",
+  REMOVE_BOOKMARK: "REMOVE_BOOKMARK",
+  SEARCH_USER: "SEARCH_USER",
+  CREATE_NEW_POST: "CREATE_NEW_POST",
+  DELETE_POST: "DELETE_POST",
+  EDIT_POST: "EDIT_POST",
+  GET_ONE_USER: "GET_ONE_USER",
+  UPDATE_FOLLOW_USER: "UPDATE_FOLLOW_USER",
+  EDIT_USER_PROFILE: "EDIT_USER_PROFILE",
+  SET_PAGE: "SET_PAGE",
+  SET_IS_NEXT_POST_LOADING: "SET_IS_NEXT_POST_LOADING",
+  GET_SINGLE_POST: "GET_SINGLE_POST",
+  ADD_NEW_COMMENT: "ADD_NEW_COMMENT",
+  DELETE_COMMENT: "DELETE_COMMENT",
+  EDIT_COMMENT: "EDIT_COMMENT",
+};
